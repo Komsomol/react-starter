1803330ff86718db2542bfb8d55c6700300b4a5b
Farhad Agzamov <farhad@thisistommy.com>
2019-01-23 17:53:54 +0000

    input field for search added

--- src/index.js
+++ src/index.js
@@ -1,17 +1,85 @@
-console.log("It lives");
+console.log("index.js called")
 
-import React, { Component } from "react";
-import ReactDOM from "react-dom";
-import { APIGetter } from "./js/api-getter";
+import React, { Component } from "react"
+import ReactDOM from "react-dom"
+import { APIGetter } from "./js/api-getter"
 
 export default class Index extends Component {
-  render() {
-    return (
-      <div>
-        <APIGetter name="The Wire" />
-      </div>
-    )
-  }
+	constructor(props){
+		super(props)
+		this.state = {
+			showname:null
+		}
+
+		this.handleReturn = this.handleReturn.bind(this)
+	}
+	
+	handleReturn(data){
+		console.log("passed data is", data) 
+		
+		this.setState({
+			showname:data
+		})
+	}
+
+	render() {
+		if(this.state.showname) {
+			return (
+				<div>
+					<APIGetter name={this.state.showname} />
+				</div>
+			)
+		} else {
+			return (
+				<div>
+					<Input callBack={this.handleReturn}/>
+				</div>
+			)
+		}
+	}
 }
 
-ReactDOM.render(<Index/>, document.getElementById("app"));
+class Input extends Component {
+	constructor(props){
+		super(props)
+		this.state = {
+			value : "",
+			placeholder: "Search for TV show"
+		}
+		
+		this.handleChange = this.handleChange.bind(this)
+		this.handleSubmit = this.handleSubmit.bind(this)
+	}
+
+	handleChange(event){
+		this.setState({
+			value: event.target.value
+		})
+	}
+
+	handleSubmit(event){
+		console.log("Submitted", this.state.value)
+		event.preventDefault()
+
+		this.props.callBack(this.state.value)
+	}
+
+	render() { 
+		return (
+			<form onSubmit={this.handleSubmit}>
+				<label>
+					<input 
+						type="text" 
+						autoComplete="true" 
+						placeholder={this.state.placeholder} 
+						value={this.state.value}
+						onChange={this.handleChange}/>
+				</label>
+				<input type="submit" value="Submit" />
+			</form>
+		)
+	}
+}
+ 
+
+ReactDOM.render(<Index/>, document.getElementById("app"))
